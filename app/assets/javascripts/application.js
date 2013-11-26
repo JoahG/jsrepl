2ca0aa8d9784c1b001b01ddd76a5777ebf2941e5
Joah Gerstenberg <joahd@gbergfamily.com>
2013-11-26 15:10:30 -0600

    Added javascripts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,38 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+console.log = function(m) {
+	$("#output").append("<span class='line'>"+m+"</span>")
+}
+
+$(document).ready(function() {
+	$("#input").focus();
+
+	$("#run").click(function(){
+		if ($("#input").val().length > 0) {
+			$(".removable").remove()
+		}
+		try {
+			eval($("#input").val())
+		} catch(err) {
+			console.log(err)
+		}
+	});
+
+	$("#save").click(function() {
+		$("#codebit_form").submit();
+	})
+
+	$(document).delegate('#input', 'keydown', function(e) {
+		var keyCode = e.keyCode || e.which;
+		if (keyCode == 9) {
+			e.preventDefault();
+			var start = $(this).get(0).selectionStart;
+			var end = $(this).get(0).selectionEnd;
+			$(this).val($(this).val().substring(0, start) + "\t" + $(this).val().substring(end));
+			$(this).get(0).selectionStart =
+			$(this).get(0).selectionEnd = start + 1;
+		}
+	});
+});
