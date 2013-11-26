8d786b41f10d06c5a440a7c2c91134bce4237653
Joah Gerstenberg <joahd@gbergfamily.com>
2013-11-26 17:21:34 -0600

    Added changes from heroku branch

--- script.js
+++ script.js
@@ -3,19 +3,26 @@ console.log = function(m) {
 }
 
 $(document).ready(function() {
-	$("#input").focus();
+	sizeBoxes = function() {
+		$("#codebit_code, #output").height($(window).height()-55)
+	}
+
+	sizeBoxes()
+
+	$("#codebit_code").focus();
+
 	$("#run").click(function(){
-		if ($("#input").val().length > 0) {
+		if ($("#codebit_code").val().length > 0) {
 			$(".removable").remove()
 		}
 		try {
-			eval($("#input").val())
+			eval($("#codebit_code").val())
 		} catch(err) {
 			console.log(err)
 		}
 	});
 
-	$(document).delegate('#input', 'keydown', function(e) {
+	$(document).delegate('#codebit_code', 'keydown', function(e) {
 		var keyCode = e.keyCode || e.which;
 		if (keyCode == 9) {
 			e.preventDefault();
@@ -26,4 +33,6 @@ $(document).ready(function() {
 			$(this).get(0).selectionEnd = start + 1;
 		}
 	});
+
+	$(window).resize(function(){sizeBoxes()})
 });
